9f9be5b3e425f6ad390261fdd25d66d161d1045b
S1bu <Sibusisodyan@gmail.com>
2023-08-12 11:52:41 +0200

    modelUser

--- api/model/Users.js
+++ api/model/Users.js
@@ -1,3 +1,71 @@
-// importing
-const db = require("../config")
-db
+const db = require ("../config")
+// This will have all the functionality
+class Users{
+    fetchUsers(req, res){
+        const query =`
+        SELECT userID, firstName, lastName, gender, userDOB,
+        emailAdd, profileURL
+        FROM Users; `
+        db.query(query,
+             (err, results) => {
+                if(err) throw err
+                res.json({
+                    status: res.statusCode,
+                    results
+                })
+             } )
+    }
+    fetchUser(req, res){
+        const query = `
+        SELECT userID, firstName, lastName, gender, userDOB,
+        emailAdd, profileURL
+        FROM Users
+        where userID = ${req.params.id};
+        `
+        db.query(query,
+            (err, result) => {
+               if(err) throw err
+               res.json({
+                   status: res.statusCode,
+                   results
+               })
+            } )
+    }
+    login(req,res){
+        const query = ``
+    }
+    register(req,res){
+
+    }
+    updateUser(req,res){
+    const query =`
+        UPDATE Users
+        SET?
+        WHERE userID =?
+        `
+        db.query(query,[req,body, req.params],
+            (err) => {
+                if(err) throw err
+                res.json({
+                    status: res.statusCode,
+                    msg:"The user record was updated."
+                })
+            })
+    }
+    deleteUser(req,res){
+        const query =
+         `
+        DELETE FROM Users
+        WHERE userID = ${req.params.id};
+        `
+        db.query(query, (err) => {
+            if(err) throw err
+            res.json({
+        status:res.statusCode,
+        msg:'The user records were deleted.'
+        })
+        })
+    }
+}
+
+module.exports =  Users  
