91a7a46dff1d03039d5f28e07a9ffa4edbea16c4
S1bu <Sibusisodyan@gmail.com>
2023-08-14 12:50:08 +0200

    done with modelUserUpdate

--- api/model/Users.js
+++ api/model/Users.js
@@ -1,5 +1,8 @@
 const db = require ("../config")
 // This will have all the functionality
+const {hash, compare, hashSync} = require ('bcrypt')
+const {createToken} = require('../middleware/AuthenticateUser')
+
 class Users{
     fetchUsers(req, res){
         const query =`
@@ -34,8 +37,43 @@ class Users{
     login(req,res){
         const query = ``
     }
-    register(req,res){
+   async register(req,res){
+        const data = req.body
+        // Encrypt password
+        data.userPass = hash(data.userPass,15)
+        //Payload
+        const user = {
+            emailAdd:data.emailAdd,
+            userPass:data.userPass
+        }
+        //creating user detail
+        //Query
+        const query = `
+        INSERT INTO Users
+        SET ?
+        `
+        // if you dont wanna use set ypu can use 'values(?,?,?,?,?,?,?,?)'
+        db.query(query,[data],(err) => {
+            if(err) throw err
+            // create a Token
+            let token = createToken(user)
+            res.cookie("LegitUser", token, 
+            {
+                maxAge: 3600000,
+                httpOnly: true
+            });
+            res.json ({
+                status: res.statusCode,
+                msg: "You are now registered"
+            })
+        })
 
+        // db.query(query, (err) => {
+        //     if(err) throw err
+        //     res.cookie()
+        // })
+
+        // async register(rew)
     }
     updateUser(req,res){
     const query =`
